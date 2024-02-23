775cdeb268dd120a5d53c0dfe827e93319df366e
bianavic <bianavic@gmail.com>
2024-02-23 18:41:50 -0300

    feat: implement method to find user by email

--- src/main/java/org/edu/usermanagementapplication/repositories/UserRepository.java
+++ src/main/java/org/edu/usermanagementapplication/repositories/UserRepository.java
@@ -3,5 +3,9 @@ package org.edu.usermanagementapplication.repositories;
 import org.edu.usermanagementapplication.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+
 }
